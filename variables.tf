aafc2e60636639f649e37f5f8ce0f42d03196990
NITIN TEOTIA <thoughtworks.teotia@extaccount.com>
2024-12-02 03:01:09 +0530

    deploy s3

--- variables.tf
+++ variables.tf
@@ -255,3 +255,13 @@
 #  type        = string
 #  default     = "development"
 #}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+variable "AWS_SESSION_TOKEN" {
+  type = string
+}
