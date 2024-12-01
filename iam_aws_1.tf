ff2c1fa66902eb839ab515f879384a7eeeb26e8c
NITIN TEOTIA <thoughtworks.teotia@extaccount.com>
2024-12-02 03:19:43 +0530

    deploy s3

--- iam_aws_1.tf
+++ iam_aws_1.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region     = "eu-central-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
+  token = var.AWS_SESSION_TOKEN
 }
 
 #resource "aws_instance" "env0_instance" {
--- variables.tf
+++ variables.tf
@@ -262,3 +262,6 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
+variable "AWS_SESSION_TOKEN" {
+  type = string
+}
